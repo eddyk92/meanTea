4f5ff5adcb5a464a0131acd75826e1c77835af9b
gregsim92 <gregsim92@gmail.com>
2016-02-03 15:47:55 -0800

    reformatted json file and had data render in a table on the main page

--- js/controllers.js
+++ js/controllers.js
@@ -1,21 +1,20 @@
 app.controller('mainController', function($scope, $http, $location){
 
-	$scope.teaSearch = function(){
-		$http.get("/data.js" + $scope.tea + '&plot=short&r=json')
+		$http.get('../data.json')
 		.then(function(response){
-			console.log(response.data)
-			$scope.data = response.data.Search 
+			$scope.teaList = response.data
 		})
-	}
 
-	$scope.teaResult = function(id){
-		$http.get('/data.js' + id).then(function(response){
-			$scope.foo = response.data;
-			$scope.foo1 = id;
-			$location.path("/" + id + '/shoppingCart')
-		})
-	}
-})
+	})
+
+	// $scope.teaResult = function(id){
+	// 	$http.get('/data.js' + id).then(function(response){
+	// 		$scope.foo = response.data;
+	// 		$scope.foo1 = id;
+	// 		$location.path("/" + id + '/shoppingCart')
+	// 	})
+	// }
+// })
 
 app.controller('secondController', function($scope, $http, $location){
 	$scope.teaId = $location.$$path.split('/')[1];
